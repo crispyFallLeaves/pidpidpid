8e6d280e4bfbd4fc851e033e74672fbcb9f6edd8
crispyFallLeaves <reflectedprism@gmail.com>
2026-08-20 18:01:01 -0700

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vite.dev
 export default defineConfig({
   plugins: [react()],
+  base: '/pidpidpid/', // 👈 Add your exact GitHub repo name here
 })
