187fb98fca39d02362e19e92680b768c38417e22
crispyFallLeaves <reflectedprism@gmail.com>
2026-08-24 14:19:26 -0700

    Updates

--- assets/index-BAY6I6j6.js
+++ assets/index-BAY6I6j6.js
@@ -19,7 +19,7 @@ Please change the parent <Route path="${e}"> to <Route path="${e===`/`?`*`:`${e}
         error = setpoint – sensor value;
         integral = integral + error;
         if (error = 0 or passes setpoint)
-        integral = 0;
+            integral = 0;
         if (error is outside useful range)
             integral = 0;
         derivative = error – prevError;
